6a46d620afc8ef2a0d17d9a4bb7cb9c91ffe055e
Dujuniorrr <durvaljunior117@gmail.com>
2023-02-16 11:51:48 -0300

    add index, about and templates styles

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, AboutView
 
 urlpatterns = [
     # path('endereço/', view.as_view(), name='nome-da-url'),
     path('', IndexView.as_view(), name='index'),
+    path('sobre/', AboutView.as_view(), name='about'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,5 @@ from django.views.generic import TemplateView
 class IndexView(TemplateView):
     template_name = "pages/index.html"
     
+class AboutView(TemplateView):
+    template_name = "pages/about.html"
